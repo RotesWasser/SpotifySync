4f2c427ffbfcbc4d223b8b3a1d34765c1a31a0cf
RotesWasser <hallo@roteswasser.com>
2020-09-29 17:12:36 +0200

    Add diffing tests

--- src/test/kotlin/com/roteswasser/spotifysync/LongestCommonSubsequenceTest.kt
+++ src/test/kotlin/com/roteswasser/spotifysync/LongestCommonSubsequenceTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 
 class LongestCommonSubsequenceTest {
     @Test
-    fun wikipediaExampleCorrect() {
+    fun `LCS example from Wikipedia is computed correctly`() {
         // See: https://en.wikipedia.org/wiki/Longest_common_subsequence_problem#Solution_for_two_sequences
         val lcs = computeLCS(listOf("A", "G", "C", "A", "T"), listOf("G", "A", "C"))
 
